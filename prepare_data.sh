11ef320f19f77da900cd0d9ca4e9a66b79f1f8f6
Thiago de Barros Lacerda <thiagotbl@gmail.com>
2014-07-20 19:45:46 -0300

    Change prepare_data.sh to deal with kernel experiments output
    Signed-off-by: Thiago de Barros Lacerda <thiagotbl@gmail.com>

--- prepare_data.sh
+++ prepare_data.sh
@@ -2,6 +2,11 @@
 
 echo -e "nr_reads\tnr_writes\tnr_ops" > tmp.txt
 
-awk '{print $16"\t"$18"\t"$20}' $1 >> tmp.txt
+if [ "$2" == "kernel" ]
+then
+    awk '{print substr($6, 0, length($6)-1)"\t"$8"\t"substr($6, 0, length($6)-1) + $8}' $1 > tmp.txt
+else
+    awk '{print $16"\t"$18"\t"$20}' $1 >> tmp.txt
+fi
 
 mv tmp.txt $1
